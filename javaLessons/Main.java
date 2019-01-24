dc14e3230c65c6a58001c255b605f60c9b248123
ilopez0127 <46633181+ilopez0127@users.noreply.github.com>
2019-01-23 19:54:22 -0800

    functions #2

---
+++ javaLessons/Main.java
@@ -0,0 +1,56 @@
+class Main
+{  
+    static double addNumber(double... numbers) 
+    {
+    //takes any amount of numbers and adds together
+    double result = 0.0;
+    for(int i =0;i<myArray.length;i++)
+    {
+    }
+    }
+    static double divideNumber (int g, int h)
+    {
+        return g
+    }
+
+    static int subtractNumber (int r, int s)
+    {
+        return r-s;
+    }
+      
+    static int addNumbers (int a, int b)
+    {
+        return a+b;
+    }
+    static int multiplyNumber(int x, int y)
+    {
+        int result = x*y;
+        return result;
+
+    
+
+    }
+    static void printPyramid(int num)
+    {
+     //takes a number and prints a pyramid up to that value
+    for(int i=0; i<num;i++)
+    {
+        for(int a=0; a<=1 ;a++)
+    {
+   
+        System.out.print(a+1); 
+       }
+       System.out.println();
+    }
+    }
+
+  public static void main(String[] args)
+  {
+      System.out.println(multiplyNumber(5,2));
+      System.out.println(addNumbers(8,8));
+      System.out.println(subtractNumber(4,1));
+  }
+}
+
+
+  
